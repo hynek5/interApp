aba3f081a934e721ebafac9e1bfbeb60928ca062
Hynek Hrabik <hynek.hrabik@microfocus.com>
2020-12-13 20:38:41 +0100

    Set email address as unique attr

--- src/main/java/com/microfocus/interApp/repositories/UserRepository.java
+++ src/main/java/com/microfocus/interApp/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.microfocus.interApp.repositories;
 import com.microfocus.interApp.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -12,6 +14,14 @@ public interface UserRepository extends CrudRepository<User, Long> {
    default Stream<User> findByName(String name) {
        Iterable<User> users = this.findAll();
        return StreamSupport.stream(users.spliterator(), false).filter((user -> Objects.equals(user.getName(),name)));
+   }
+
+   default Set<String> findAllExistingEmails() {
+       Set<String> existingEmails = new HashSet<>();
+       this.findAll()
+               .forEach((user) -> user.getMailAccounts()
+                       .forEach((mailAccount -> existingEmails.add(mailAccount.getFullName()))));
+       return existingEmails;
    }
 
 }
--- src/main/java/com/microfocus/interApp/services/UserService.java
+++ src/main/java/com/microfocus/interApp/services/UserService.java
@@ -3,7 +3,6 @@ package com.microfocus.interApp.services;
 import com.microfocus.interApp.domain.MailAccount;
 import com.microfocus.interApp.domain.User;
 import com.microfocus.interApp.repositories.UserRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,17 +19,23 @@ public class UserService implements DataService<User> {
 
     @Override
     public String insertToDb(User candidate) {
-        List<User> usersWithTheSameName = userRepository.findByName(candidate.getName()).collect(Collectors.toList());
-        if (usersWithTheSameName.size() == 0) {
-            //no such user exist add to the table
-            candidate.setMailAccounts();
-            userRepository.save(candidate);
-            return "User has been added";
+        if (candidate.getName().isEmpty() || candidate.getMailAccount().isEmpty()) {
+            return "Please fill up all required fields";
         }
 
+        if (userRepository.findAllExistingEmails().contains(candidate.getMailAccount())) {
+            return "Email address:" + candidate.getMailAccount() + " already registered, pick a different one.";
+        }
+
+        List<User> usersWithTheSameName = userRepository.findByName(candidate.getName()).collect(Collectors.toList());
+        if (usersWithTheSameName.size() == 0) {
+                //no such user exist add to the table
+                candidate.setMailAccounts();
+                userRepository.save(candidate);
+                return "User has been added";
+        }
         if (usersWithTheSameName.size() == 1) {
             //user with the same name exists so add his new email account
-            MailAccount testMailAcc = new MailAccount(candidate.getMailAccount());
             if (usersWithTheSameName.get(0).getMailAccounts().add(new MailAccount(candidate.getMailAccount()))) {
                 userRepository.save(usersWithTheSameName.get(0));
                 return "User " + candidate.getName() + " has been updated";
